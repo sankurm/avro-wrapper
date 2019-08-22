fd781dd4fb46bb1acd4e8ee2b1a87df05550afb1
Ankur Satle <sankurm@users.noreply.github.com>
2019-08-22 19:50:33 +0530

    Brought from playpen/avro-wrapper

---
+++ avro-helper.h
@@ -0,0 +1,22 @@
+#ifndef _AVRO_HELPER_H_
+#define _AVRO_HELPER_H_
+
+#include "GenericWriter.hh"
+#include <utility>
+
+class GenericWriter : avro::GenericWriter
+{
+	using avro::GenericWriter(const ValidSchema& s, const EncoderPtr& encoder);
+	//Not including static functions here - they can come later, if needed
+	
+	using GenericUnionDatum = std::pair<const avro::GenericUnion&, const avro::GenericDatum&>;
+	void write(const GenericUnionDatum datum) const;
+};
+
+void GenericWriter::write(const GenericUnionDatum& union_datum) const {
+	//datum.selectBranch(union_datum.first.encodeUnionIndex());
+	encoder_.encodeUnionIndex(union_datum.first.unionBranch());	//TODO: encoder_ is not accessible here!
+	return write(union_datum.second);
+}
+
+#endif  //_AVRO_HELPER_H_
